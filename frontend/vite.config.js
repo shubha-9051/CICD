179a4a00ce64bfa5f0835735cb35cbefd6095b81
Shubhajit <shubhajit2003@gmail.com>
2025-09-11 01:42:44 +0530

    docker added

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // Specify the development server port
+    port: 3001,
+  },
+  // Base name of your app
+  base: "/", // Replace this with the subdirectory path if needed
+});
